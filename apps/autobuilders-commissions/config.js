588ce3a48639000be794165d177af6442f4e2867
MS2110 <108090719+MS2110@users.noreply.github.com>
2025-11-08 22:01:54 +0100

    first deploy

---
+++ apps/autobuilders-commissions/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clientID: "548e5347ad3baa84",
+  clientSecret: "940ee5f5ee795030105f6d7ddb13cfb44afad190",
+  callbackURL:
+    "https://cool-unicorn-d2287f.netlify.app/auth/pipedrive/callback",
+};
