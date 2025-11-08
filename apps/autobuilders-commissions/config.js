4684f7507205d7975dc3a519745e7e3ddf78f257
MS2110 <108090719+MS2110@users.noreply.github.com>
2025-11-08 22:06:25 +0100

    Update config.js

--- apps/autobuilders-commissions/config.js
+++ apps/autobuilders-commissions/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  clientID: "548e5347ad3baa84",
-  clientSecret: "940ee5f5ee795030105f6d7ddb13cfb44afad190",
-  callbackURL:
-    "https://cool-unicorn-d2287f.netlify.app/auth/pipedrive/callback",
+  clientID: process.env.CLIENT_ID || "",
+  clientSecret: process.env.CLIENT_SECRET || "",
+  callbackURL: process.env.CALLBACK_URL || "",
 };
